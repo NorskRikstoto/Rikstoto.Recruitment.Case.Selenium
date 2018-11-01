81ca2ea7e5825c07be851d9a375796027566f9bf
Roger Hoem-Martinsen <roger.h.martinsen@gmail.com>
2018-11-01 16:04:54 +0100

    a possible solution

--- tests/frontpage_tests.py
+++ tests/frontpage_tests.py
@@ -1,18 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def test_raceday_timeline_heading_is_correct(selenium):
     selenium.get("https://beta.rikstoto.no")
-    element = selenium.find_element_by_class_name("race-day-timeline__header__content__title")
+    element = selenium.find_element(By.CLASS_NAME, "race-day-timeline__header__content__title")
     assert element.text == "Bane- og løpsoversikt"
 
 def test_raceday_timeline_is_not_empty(selenium):
-    '''Make a test that verifies that the raceday timeline is not empty'''
-    assert False
+    selenium.get("https://beta.rikstoto.no")
+    elements = selenium.find_elements(By.CLASS_NAME, "timeline-race-cell__race-number")
+    assert len(elements) > 0
 
 def test_can_navigate_via_top_menu_to_spill(selenium):
-    '''
-    Make a test that verifies that the user 
-    is able to navigate from the frontpage 
-    to the "spill"-page (via the top-manu "spill" -> "Løpsoversikt") and
-    verify by finding the text "Baneoversikt" '''
-    assert False
+    selenium.get("https://beta.rikstoto.no")
+    selenium.find_element(By.CSS_SELECTOR, "product-menu").click()
+    selenium.find_element(By.PARTIAL_LINK_TEXT , "Løpsoversikt").click()
+    headings = selenium.find_elements(By.CSS_SELECTOR, "h3")
+    assert any('Baneoversikt' in element.text for element in headings)
